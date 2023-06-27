cd368884917ad474ffaf3ffbe13264ec50d6b86d
alex <i202130378@cibertec.edu>
2023-06-27 00:36:43 -0500

    funcionalidad de lista de productos para administrador

--- src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,12 @@ public class ProductoServiceImpl implements ProductoService {
 	@Override
 	public void delete(Integer id) {
 		productoRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Producto> findAll() {
+		
+		return productoRepository.findAll();
 	}
 
 }
